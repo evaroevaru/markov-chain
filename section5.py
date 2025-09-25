4546ef93bb1b1edef9061de9a1048692d4c3901a
evaroevaru <16yprism@gmail.com>
2025-09-25 14:25:26 +0800

    Section 5 - save 2
    Save 2 of section 5. Menu design is complete, and I've started work on some calculations.

--- section5.py
+++ section5.py
@@ -6,24 +6,59 @@ from ttkbootstrap.constants import *
 root = tk.Tk()
 root.geometry("500x300")
 root.resizable(True, True)
-b1 = ttk.Button(root, text="Button 1", bootstyle=SUCCESS)
-b1.place(relx=0.4, rely=0.6, anchor=CENTER)
+root.minsize(400, 200)
+calc_btn = ttk.Button(root, text="Calculate", bootstyle=SUCCESS)
+calc_btn.place(relx=0.4, rely=0.6, anchor=CENTER)
 
-b2 = ttk.Button(root, text="Button 2", bootstyle=(INFO, OUTLINE))
-b2.place(relx=0.6, rely=0.6, anchor=CENTER)
 
-b3 = ttk.Button(root, text="Quit", bootstyle=(DANGER), command=root.destroy)
-b3.place(relx=0.5, rely=0.8, anchor=CENTER)
+quit_btn = ttk.Button(root, text="Quit", bootstyle=(DANGER), command=root.destroy)
+quit_btn.place(relx=0.6, rely=0.6, anchor=CENTER)
+
+dropdown_lbl = ttk.Label(text="Select initial router: ")
+dropdown_lbl.place(relx=0.33, rely=0.4, anchor=CENTER)
 
 n = tk.IntVar()
-dropdown = ttk.Combobox(root, textvariable=n)
+dropdown = ttk.Combobox(root, textvariable=n, state='readonly', bootstyle=(INFO))
 dropdown['values'] = ('A', 'B', 'C', 'D', 'E')
-dropdown.place(relx=0.5, rely=0.4, anchor=CENTER)
+dropdown.place(relx=0.67, rely=0.4, anchor=CENTER)
 dropdown.set("A")
 
+entry_lbl = ttk.Label(text="Enter number of days: ")
+entry_lbl.place(relx=0.33, rely=0.2, anchor=CENTER)
 
 nodays = tk.IntVar()
 input_entry = ttk.Entry(root, textvariable=nodays)
-input_entry.place(relx=0.5, rely=0.2, anchor=CENTER)
+input_entry.place(relx=0.67, rely=0.2, anchor=CENTER)
+
+# main program
+e = 0
+f = 0
+g = 0
+h = 0
+i = 0
+with open('network_traffic.txt','r') as file:
+    for line in file:
+        d = line.split('->')
+        if d[0]=='A':
+            e = e+1
+        if d[0]=='B':
+             f = f+1
+        if d[0]=='C':
+            g = g+1
+        if d[0]=='D':
+            h = h+1
+        if d[0]=='E':
+            i = i +1
+e = round(e/10000,5)
+f = round(f/10000,5)
+g = round(g/10000,5)
+h = round(h/10000,5)
+i = round(i/10000,5)
+
+print(str(e))
+print(str(f))
+print(str(g))
+print(str(h))
+print(str(i))
 
 root.mainloop()
